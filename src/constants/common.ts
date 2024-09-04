e9004bdccd9657b9971b2ea188baff34920b3eba
AtanUncensored <jonathanruba8@gmail.com>
2024-09-04 08:55:33 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Jonathan V. Ruba';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Hello Im Jonathan Ruba, a graduating student in Bachelor of Science in Information Technology (BSIT) at Mater Dei College. I have a strong passion for technology, especially in software development and web design.';
+export const ABOUT_TEXT_2 =
+  'Throughout my studies, Ive gained skills in various programming languages, web development, and database management. Im excited to apply my knowledge and contribute to innovative projects in the tech industry.  ';
+export const ABOUT_TEXT_3 =
+  'Throughout my studies, Ive built several web applications using Laravel, leveraging its powerful features to create robust and scalable solutions. I enjoy solving complex problems and am always eager to learn new technologies and improve my skills.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'SI am a graduating Bachelor of Science in Information Technology (BSIT) student with a strong focus on web development. Proficient in Laravel and experienced in building dynamic and responsive web applications. I have a solid understanding of both front-end and back-end technologies, with a passion for creating clean, efficient code and user-friendly interfaces.';
+export const RESUME_TEXT_2 =
+  'I am a graduating Bachelor of Science in Information Technology (BSIT) student with a strong focus on web development. Proficient in Laravel and experienced in building dynamic and responsive web applications.';
